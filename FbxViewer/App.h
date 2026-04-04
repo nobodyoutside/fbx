622fffca8555bf87cd690559d32d6cb32815b3f4
nobody45Window <nobodyoutside@outlook.com>
2026-04-04 23:22:15 +0900

    Step: after file modification

---
+++ FbxViewer/App.h
@@ -0,0 +1,44 @@
+#ifndef APP_H
+#define APP_H
+
+#include <GLFW/glfw3.h>
+#include "FbxLoader.h"
+#include "Renderer.h"
+
+/// @brief GLFW 윈도우 + ImGui + 렌더러를 통합 관리하는 애플리케이션 클래스.
+/// 메인 루프, 입력 처리, ImGui UI 패널을 담당한다.
+class App {
+public:
+    App();
+    ~App();
+
+    int run();
+
+private:
+    bool initWindow();
+    void mainLoop();
+    void drawImGuiPanel();
+    void openFileDialog();
+
+    static void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
+    static void cursorPosCallback(GLFWwindow* window, double xpos, double ypos);
+    static void scrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+
+    GLFWwindow* mWindow = nullptr;
+    FbxLoader mLoader;
+    Renderer mRenderer;
+
+    // UI 상태
+    int mViewModeIndex = 0;
+    float mAnimTime = 0.0f;
+    bool mFileLoaded = false;
+    char mFilePath[512] = {};
+
+    // 마우스 상태
+    bool mDraggingLeft = false;
+    bool mDraggingMiddle = false;
+    double mLastMouseX = 0.0;
+    double mLastMouseY = 0.0;
+};
+
+#endif // APP_H

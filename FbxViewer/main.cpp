f229019d4ca4ef888004a4bd324c4a2f729a4696
nobody45Window <nobodyoutside@outlook.com>
2026-04-04 23:22:47 +0900

    Step: after file modification

--- FbxViewer/main.cpp
+++ FbxViewer/main.cpp
@@ -1,12 +1,6 @@
-﻿#include <QApplication>
-#include "MainWindow.h"
+#include "App.h"
 
-/// @brief FBX 뷰어 애플리케이션의 진입점.
-/// QApplication을 생성하고, 메인 윈도우를 표시한 뒤 Qt 이벤트 루프를 실행한다.
-int main(int argc, char* argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+int main() {
+    App app;
+    return app.run();
 }
